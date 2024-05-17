60392239114a6ec2e5c4272837a69712e71aa3e9
Priyansh-14 <priyanshja2003@gmail.com>
2024-05-17 20:07:32 +0530

    React Practice

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Practicing_React",
+  base: "./Practicing_React",
   plugins: [react()],
 });

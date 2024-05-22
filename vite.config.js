683ad64f82ee72c91a90be358e8d904907f7ee8a
Priyansh-14 <priyanshja2003@gmail.com>
2024-05-22 17:40:05 +0530

    updated readme.md

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./Practicing_React",
+  base: "./First-React-Project",
   plugins: [react()],
 });
